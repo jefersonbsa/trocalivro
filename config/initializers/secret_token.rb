46967ecb157626f19917a8183ec77f97d3328de7
Jeferson Sá <jeferson.bsa@gmail.com>
2012-03-23 18:41:34 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trocalivro::Application.config.secret_token = '6fd3e29fcca43c20f9fc411f9423bbd0c441083b0040988a699ce52c0d91e5e188c2de94bdc5f0c570f146a09562619eae79d48c680391bb67f7c2a18c537960'
